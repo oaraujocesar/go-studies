6ca95a1a407d8e22d59fa82cf99e9ce8ef5d7160
César O. Araújo <oaraujocesar@gmail.com>
2022-01-09 11:25:09 -0300

    refactor: :recycle: Making the test more generic

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,13 +3,20 @@ package dictionary
 import "testing"
 
 func TestSearch(t *testing.T) {
+
+	assertStrings := func(t testing.TB, received, expected, word string) {
+		t.Helper()
+
+		if received != expected {
+			t.Errorf("Received %q, but expected %q given %q", received, expected, word)
+		}
+	}
+
 	dictionary := map[string]string{"test": "this is just a test"}
 
 	word := "test"
 	received := Search(dictionary, word)
 	expected := "this is just a test"
 
-	if received != expected {
-		t.Errorf("Received %q, but expected %q given %q", received, expected, word)
-	}
+	assertStrings(t, received, expected, word)
 }
