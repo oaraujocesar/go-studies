2ee2040cf3b9205e8265b6bf0f83c8810d91ef46
César O. Araújo <oaraujocesar@gmail.com>
2022-01-18 00:03:58 -0300

    feat: :recycle: Showing index and arg line by line

--- blue-book/ch1/echo/main.go
+++ blue-book/ch1/echo/main.go
@@ -10,4 +10,8 @@ import (
 func main() {
 	fmt.Println("Running ", os.Args[0])
 	fmt.Println(strings.Join(os.Args[1:], " "))
+
+	for index, arg := range os.Args[1:] {
+		fmt.Println(index, arg)
+	}
 }
