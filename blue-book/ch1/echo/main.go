9e5ba313cf546f5770effc9790afc43deb6a1c30
César O. Araújo <oaraujocesar@gmail.com>
2022-01-17 23:55:03 -0300

    refactor: :recycle: Using range to iterate on args

--- blue-book/ch1/echo/main.go
+++ blue-book/ch1/echo/main.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	var s, sep string
+	s, sep := "", ""
 
-	for i := 0; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
 		sep = " "
 	}
 
