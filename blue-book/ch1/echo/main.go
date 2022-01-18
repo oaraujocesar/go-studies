a7f812abc86e72c164553617478d86b66b910313
César O. Araújo <oaraujocesar@gmail.com>
2022-01-18 00:30:28 -0300

    test: :white_check_mark: Testing the execution time for all manners of doing echo

--- blue-book/ch1/echo/main.go
+++ blue-book/ch1/echo/main.go
@@ -5,13 +5,61 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	fmt.Println("Running ", os.Args[0])
-	fmt.Println(strings.Join(os.Args[1:], " "))
 
 	for index, arg := range os.Args[1:] {
 		fmt.Println(index, arg)
 	}
+
+	echo1()
+
+	echo2()
+
+	echo3()
+
+	fmt.Printf("enlapsed %.6fs", time.Since(start).Seconds())
+}
+
+func echo1() {
+	start := time.Now()
+
+	var s, sep string
+
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+
+	secs := time.Since(start).Seconds()
+
+	fmt.Printf("echo1 - %.7fs %s\n", secs, s)
+}
+
+func echo2() {
+	start := time.Now()
+	s, sep := "", ""
+
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+
+	secs := time.Since(start).Seconds()
+
+	fmt.Printf("echo2 - %.7fs %s\n", secs, s)
+}
+
+func echo3() {
+	start := time.Now()
+
+	s := strings.Join(os.Args[1:], " ")
+
+	secs := time.Since(start).Seconds()
+
+	fmt.Printf("echo3 - %.7fs %s\n", secs, s)
 }
