f93b72eafaae8a78995e13353176f45bcaf0b1d2
César O. Araújo <oaraujocesar@gmail.com>
2022-01-19 20:53:32 -0300

    :tada: First version of dup

---
+++ blue-book/ch1/dup/go.mod
@@ -0,0 +1,3 @@
+module github.com/oaraujocesar/go-studies/blue-book/ch1/dup
+
+go 1.17
---
+++ blue-book/ch1/dup/main.go
@@ -0,0 +1,22 @@
+// Dup shows the text of every duplicated line
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
