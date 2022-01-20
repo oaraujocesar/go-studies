ac941eeeab08fecb607a5e51d240761c03ec5299
César O. Araújo <oaraujocesar@gmail.com>
2022-01-19 21:02:33 -0300

    refactor: :recycle: Read list of lines from a file or stdin

---
+++ blue-book/ch1/dup/README.md
@@ -0,0 +1,1 @@
+## remember to use CTRL + D to finish the loop and see the results
--- blue-book/ch1/dup/main.go
+++ blue-book/ch1/dup/main.go
@@ -8,15 +8,56 @@ import (
 )
 
 func main() {
+	// dup1()
+
+	dup2()
+}
+
+func dup1() {
 	counts := make(map[string]int)
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		counts[input.Text()]++
 	}
-	// NOTE: ignoring potential errors from input.Err()
+
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
+	}
+}
+
+func dup2() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			file, err := os.Open(arg)
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			}
+
+			countLines(file, counts)
+
+			defer file.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+
+	for input.Scan() {
+		counts[input.Text()]++
 	}
 }
