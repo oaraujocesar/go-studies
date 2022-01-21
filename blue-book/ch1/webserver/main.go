ba7ade0a924cc3a56ad3033f9c5cef11d5338aad
César O. Araújo <oaraujocesar@gmail.com>
2022-01-21 16:46:59 -0300

    refactor: :recycle: Showing some meta information about the requested address

--- blue-book/ch1/webserver/main.go
+++ blue-book/ch1/webserver/main.go
@@ -22,7 +22,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	count++
 	mu.Unlock()
 
-	fmt.Fprintf(w, "URL.path = %q", r.URL.Path)
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+
+	for key, value := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", key, value)
+	}
+
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for key, value := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", key, value)
+	}
+
 }
 
 func countHandler(w http.ResponseWriter, r *http.Request) {
