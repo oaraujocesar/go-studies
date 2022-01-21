35f3bcc4f3a0866b1b3ff77cec4b0225e6e23d28
César O. Araújo <oaraujocesar@gmail.com>
2022-01-21 16:40:46 -0300

    feat: :sparkles: Counting the number of visits to a certain path

--- blue-book/ch1/webserver/main.go
+++ blue-book/ch1/webserver/main.go
@@ -4,14 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+var mu sync.Mutex
+var count int
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", countHandler)
 
 	log.Fatal(http.ListenAndServe("localhost:3000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.path = %q\n", r.URL.Path)
+	mu.Lock()
+	count++
+	mu.Unlock()
+
+	fmt.Fprintf(w, "URL.path = %q", r.URL.Path)
+}
+
+func countHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+
+	fmt.Fprintf(w, "Count %d\n", count)
+
+	mu.Unlock()
 }
