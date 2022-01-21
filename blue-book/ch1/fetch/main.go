793c8248c6403413500ab7f4db98e6fc70c9be17
César O. Araújo <oaraujocesar@gmail.com>
2022-01-20 21:19:32 -0300

    feat: :tada: Create fetch to get body from urls

---
+++ blue-book/ch1/fetch/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		res, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+
+		defer res.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", body)
+	}
+}
