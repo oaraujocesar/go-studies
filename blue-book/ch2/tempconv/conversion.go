91eb353e6f2b3c46f2222ceefca6f7446b8007a9
César O. Araújo <oaraujocesar@gmail.com>
2022-01-25 23:45:25 -0300

    feat: :sparkles: Conversion between scales using types

---
+++ blue-book/ch2/tempconv/conversion.go
@@ -0,0 +1,18 @@
+package tempconv
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func CelsiusToFahrenheit(c Celsius) (f Fahrenheit, inputC Celsius) {
+	return Fahrenheit(c*9/5 + 32), c
+}
+
+func FahrenheitToCelsius(f Fahrenheit) (c Celsius, inputF Fahrenheit) {
+	return Celsius((f - 32) * 5 / 9), f
+}
