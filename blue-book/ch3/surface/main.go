0fbe84ce8507cb23a16786ef6002907914ef8457
César O. Araújo <oaraujocesar@gmail.com>
2022-01-29 22:11:49 -0300

    feat: :sparkles: rendering new figures

--- blue-book/ch3/surface/main.go
+++ blue-book/ch3/surface/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"math"
+	"os"
 )
 
 const (
@@ -10,37 +12,35 @@ const (
 	cells         = 100
 	xyrange       = 30.0
 	xyscale       = width / 2 / xyrange
-	zscale        = height * 0.2
+	zscale        = height * 0.4
 	angle         = math.Pi / 6
 )
 
-var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
-func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; strokewidth: 0.7' "+
+type zFunc func(x, y float64) float64
+
+func svg(w io.Writer, f zFunc) {
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
-
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
-
+			ax, ay := corner(i+1, j, f)
+			bx, by := corner(i, j, f)
+			cx, cy := corner(i, j+1, f)
+			dx, dy := corner(i+1, j+1, f)
 			if math.IsNaN(ax) || math.IsNaN(ay) || math.IsNaN(bx) || math.IsNaN(by) || math.IsNaN(cx) || math.IsNaN(cy) || math.IsNaN(dx) || math.IsNaN(dy) {
 				continue
 			}
-
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			fmt.Fprintf(w, "<polygon style='stroke: %s; fill: #222222' points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				"#666666", ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-
-	fmt.Println("</svg>")
+	fmt.Fprintln(w, "</svg>")
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int, f zFunc) (float64, float64) {
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
@@ -48,12 +48,36 @@ func corner(i, j int) (float64, float64) {
 
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-
 	return sx, sy
 }
 
-func f(x, y float64) float64 {
-	r := math.Hypot(x, y)
+func eggbox(x, y float64) float64 {
+	return 0.2 * (math.Cos(x) + math.Cos(y))
+}
 
-	return math.Sin(r)
+func saddle(x, y float64) float64 {
+	a := 25.0
+	b := 17.0
+	a2 := a * a
+	b2 := b * b
+	return (y*y/a2 - x*x/b2)
+}
+
+func main() {
+	usage := "usage: go run main.go saddle | eggbox"
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+	var f zFunc
+	switch os.Args[1] {
+	case "saddle":
+		f = saddle
+	case "eggbox":
+		f = eggbox
+	default:
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+	svg(os.Stdout, f)
 }
